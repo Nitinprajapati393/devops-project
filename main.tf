179edc788706af88f90a73278d1ac80d95f3d63e
Nitinprajapati393 <nitinaprajapati20@gnu.ac.in>
2024-08-12 18:55:37 +0530

    change image

--- main.tf
+++ main.tf
@@ -32,22 +32,33 @@ resource "azurerm_storage_container" "tfstate" {
 }
 
 resource "azurerm_container_group" "example" {
-  name                = "example-container-group"
+  name                = "example-continst"
   location            = azurerm_resource_group.tfstate.location
   resource_group_name = azurerm_resource_group.tfstate.name
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-label"
   os_type             = "Linux"
 
   container {
-    name   = "nginx"
-    image  = "nginx:latest"
+    name   = "hello-world"
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
     cpu    = "0.5"
     memory = "1.5"
+
     ports {
-      port     = 80
+      port     = 443
       protocol = "TCP"
     }
   }
 
-  ip_address_type = "Public"
-  dns_name_label  = "example-nginx-container"
+  container {
+    name   = "sidecar"
+    image  = "mcr.microsoft.com/azuredocs/aci-tutorial-sidecar"
+    cpu    = "0.5"
+    memory = "1.5"
+  }
+
+  tags = {
+    environment = "testing"
+  }
 }
