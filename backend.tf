1a8575577ac679a0e07c46ee96957123ba4763b6
Nitinprajapati393 <nitinaprajapati20@gnu.ac.in>
2024-08-12 10:17:33 +0530

    storage created

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name   = "example-resources"
-    storage_account_name  = "examplestorageacct"
+    storage_account_name  = "devopsprojects3cloudhub"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
