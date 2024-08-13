8f4020fd0c3c618553a9a8e4c5f14a4304847fd5
Nitinprajapati393 <nitinaprajapati20@gnu.ac.in>
2024-08-13 11:03:57 +0530

    update RG name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "example-resources"
+    resource_group_name   = "example-resources-test"
     storage_account_name  = "devopss3pstorageaccount"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
