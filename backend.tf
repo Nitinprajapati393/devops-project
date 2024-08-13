92555ae3ae94322795355a5d766016e7fc0e0f85
Nitinprajapati393 <nitinaprajapati20@gnu.ac.in>
2024-08-13 10:54:55 +0530

    update storage account name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name   = "example-resources"
-    storage_account_name  = "devopsprojects3cloudhub"
+    storage_account_name  = "devopss3pstorageaccount"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
